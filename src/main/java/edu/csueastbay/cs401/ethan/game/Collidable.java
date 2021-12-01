9cf0b8898f7a8b4d8f2fab81edcb16bbca3ec597
MeMyselfAnDie <ethan@ketell.com>
2021-11-30 18:12:02 -0800

    Adding javadoc comments

--- src/main/java/edu/csueastbay/cs401/ethan/game/Collidable.java
+++ src/main/java/edu/csueastbay/cs401/ethan/game/Collidable.java
@@ -1,8 +1,16 @@
 package edu.csueastbay.cs401.ethan.game;
 
-import javafx.geometry.Point2D;
 import javafx.scene.shape.Shape;
 
+/**
+ * A Collidable is an Object which, when added to a {@link Game}, can check for collisions with other Collidables.
+ * @see Game#getCollisions(Collidable)
+ * @see Game#getCollisionsWithType(Collidable, Class)
+ */
 public interface Collidable {
+    /**
+     * Returns the {@link Shape} used for Collisions.
+     * @return the collision shape
+     */
     Shape getCollisionShape();
 }
--- src/main/java/edu/csueastbay/cs401/ethan/game/InputHandler.java
+++ src/main/java/edu/csueastbay/cs401/ethan/game/InputHandler.java
@@ -1,5 +1,13 @@
 package edu.csueastbay.cs401.ethan.game;
 
+/**
+ * Interface for providing simple controls for a {@link Game}
+ */
 public interface InputHandler {
+    /**
+     * Returns whether the given control is being held.
+     * @param control The control to check
+     * @return if the control is being held
+     */
     boolean isHeld(String control);
 }
