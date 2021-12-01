2b030867faa21b985303463194ad4a334eba6f0f
Chan Saeteurn <csaeteurn8@horizon.csueastbay.edu>
2021-12-01 03:30:02 -0800

    adding final changes

--- src/main/java/edu/csueastbay/cs401/pong/MainMenuController.java
+++ src/main/java/edu/csueastbay/cs401/pong/MainMenuController.java
@@ -41,9 +41,9 @@ public class MainMenuController implements Initializable {
         registry = new Registry();
         registry.register("Classic Pong", "classic", "Classic Pong Game");
         registry.register("Srishti's Pong", "srishti", "Srishti's Pong Game");
-
-
         registry.register("Taylor Hansen", "thansen", "Four Way Pong Game");
+        registry.register("Chan Saeteurn", "csaeteurn", "Background image, Victory message " +
+                "and restart button");
         registry.register("Ngon Ly", "nly", "Get Closer to the monitor...");
         registry.register("Joshua Rodriguez", "StarWarsPong", "A simple Star Wars themed" +
                 " ping pong game that is fast paced with power small power zones that make the game experience interesting. ");
