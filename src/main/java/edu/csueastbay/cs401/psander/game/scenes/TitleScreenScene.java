4683a792e2d82f6f1e8efc5f0d571972d75f4230
Paul Sander <psander@horizon.csueastbay.edu>
2021-12-01 09:55:02 -0800

    Implement title screen menus and keybinding

--- src/main/java/edu/csueastbay/cs401/psander/game/scenes/TitleScreenScene.java
+++ src/main/java/edu/csueastbay/cs401/psander/game/scenes/TitleScreenScene.java
@@ -1,6 +1,12 @@
 package edu.csueastbay.cs401.psander.game.scenes;
 
+import edu.csueastbay.cs401.psander.PongWare;
+import edu.csueastbay.cs401.psander.engine.common.HorizontalAlignment;
 import edu.csueastbay.cs401.psander.engine.gameObjects.GameObject;
+import edu.csueastbay.cs401.psander.engine.math.Vector2D;
+import edu.csueastbay.cs401.psander.engine.menu.ActionItem;
+import edu.csueastbay.cs401.psander.engine.menu.MenuContainer;
+import edu.csueastbay.cs401.psander.engine.render.RectangleRenderer;
 import edu.csueastbay.cs401.psander.engine.render.TextRenderer;
 import edu.csueastbay.cs401.psander.engine.scenes.Scene;
 import edu.csueastbay.cs401.psander.game.scripts.TitleScreenManager;
@@ -10,17 +16,46 @@ public class TitleScreenScene extends Scene {
     @Override
     public void init() {
         var manager = new GameObject("manager");
-        manager.addComponent(new TitleScreenManager());
+        var managerComponent = new TitleScreenManager();
+        manager.addComponent(managerComponent);
         addGameObject(manager);
 
         var title = new GameObject("title");
-        title.Transform().Position().set(300, 300);
-        title.addComponent(new TextRenderer("PONG", Color.WHITE, 50, 0));
+        title.Transform().Position().set(PongWare.getFieldWidth() / 2, 200);
+        title.addComponent(new TextRenderer("PONG", Color.WHITE,
+                50, 0, HorizontalAlignment.CENTER));
         addGameObject(title);
 
-        var instruction = new GameObject("instruction");
-        instruction.Transform().Position().set(300, 500);
-        instruction.addComponent(new TextRenderer("Press [Space] to start", Color.WHITE, 25, 0));
-        addGameObject(instruction);
+        // Menu
+        var horizontalMenuPosition = 600;
+        var startGameOption = new GameObject("start game");
+        startGameOption.Transform().Position().set(horizontalMenuPosition, 500);
+        startGameOption.addComponent(new TextRenderer("Start Game", Color.WHITE, 20));
+        var startMenuItem = new ActionItem();
+        startMenuItem.setAction(managerComponent::TransitionToGameScreen);
+        startGameOption.addComponent(startMenuItem);
+        addGameObject(startGameOption);
+
+        var optionMenu = new GameObject("option menu");
+        optionMenu.Transform().Position().set(horizontalMenuPosition, 550);
+        optionMenu.addComponent(new TextRenderer("Options", Color.WHITE, 20));
+        var optionMenuItem = new ActionItem();
+        optionMenuItem.setAction(managerComponent::TransitionToOptionsMenu);
+        optionMenu.addComponent(optionMenuItem);
+        addGameObject(optionMenu);
+
+        var cursor = new GameObject("cursor");
+        cursor.Transform().Position().set(-50, -50);
+        cursor.addComponent(new RectangleRenderer(10, 20, Color.GREEN));
+        addGameObject(cursor);
+
+        var menuController = new GameObject("menu controller");
+        var menuContainer = new MenuContainer();
+        menuContainer.addMenuItem(startGameOption);
+        menuContainer.addMenuItem(optionMenu);
+        menuContainer.setCursor(cursor);
+        menuContainer.setOffset(new Vector2D(-15, 5));
+        menuController.addComponent(menuContainer);
+        addGameObject(menuController);
     }
 }
--- src/main/java/edu/csueastbay/cs401/psander/game/scripts/TitleScreenManager.java
+++ src/main/java/edu/csueastbay/cs401/psander/game/scripts/TitleScreenManager.java
@@ -5,17 +5,30 @@ import edu.csueastbay.cs401.psander.engine.input.InputManager;
 import edu.csueastbay.cs401.psander.engine.scenes.SceneManager;
 import edu.csueastbay.cs401.psander.engine.scripts.Script;
 import edu.csueastbay.cs401.psander.game.scenes.BasicGameScene;
+import edu.csueastbay.cs401.psander.game.scenes.OptionMenuScene;
 import javafx.scene.input.KeyCode;
 
 public class TitleScreenManager extends Script {
     @Override
-    public void update(double delta) {
-        if (InputManager.getInstance().isKeyPressed(KeyCode.SPACE)) {
-            PongWare.State().reset();
-            PongWare.State().set_winningScore(10);
-            var gameScene = new BasicGameScene();
-            gameScene.init();
-            SceneManager.getInstance().swap(gameScene);
-        }
+    public void update(double delta) { }
+
+    /**
+     * Transitions to the main game scene
+     */
+    public void TransitionToGameScreen() {
+        PongWare.State().reset();
+        PongWare.State().set_winningScore(10);
+        var gameScene = new BasicGameScene();
+        gameScene.init();
+        SceneManager.getInstance().swap(gameScene);
+    }
+
+    /**
+     * Transitions to the option screen.
+     */
+    public void TransitionToOptionsMenu() {
+        var optionsScene = new OptionMenuScene();
+        optionsScene.init();
+        SceneManager.getInstance().push(optionsScene);
     }
 }
