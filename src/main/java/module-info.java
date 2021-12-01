fd81e0073d96367b61213d6eb77b1987d19ee4b9
Paul Raupach <paul@raupach.com>
2021-12-01 11:09:04 -0800

    Cleaning up the modules-info after merging.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,8 +4,7 @@ module edu.csueastbay.cs401.pong {
     requires javafx.fxml;
     requires javafx.base;
     requires javafx.controls;
-  
-    opens edu.csueastbay.cs401.pong to javafx.fxml;
+
     opens edu.csueastbay.cs401.pong to javafx.fxml, javafx.graphics, javafx.media;
     opens edu.csueastbay.cs401.classic to javafx.fxml;
     opens edu.csueastbay.cs401.felixchoypong to javafx.fxml, javafx.graphics, javafx.media;
