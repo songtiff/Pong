974b98e18e5af608e3e96f1fc38e5fa1e61ad680
Paul Sander <psander@horizon.csueastbay.edu>
2021-12-01 06:25:24 -0800

    Bring in JavaFX media dependencies
    Required to get sound and audio working.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module edu.csueastbay.cs401.pong {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.media;
 
     opens edu.csueastbay.cs401.pong to javafx.fxml;
     opens edu.csueastbay.cs401.classic to javafx.fxml;
