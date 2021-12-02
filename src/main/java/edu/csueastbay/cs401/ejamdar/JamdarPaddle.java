55dd93fc3b0dde4647f464a22a2d014faae732fd
Paul Raupach <paul@raupach.com>
2021-12-01 17:07:59 -0800

    Reset the paddle back to the original and modified ejamdar code by adding its own paddle.

--- src/main/java/edu/csueastbay/cs401/ejamdar/JamdarPaddle.java
+++ src/main/java/edu/csueastbay/cs401/ejamdar/JamdarPaddle.java
@@ -1,22 +1,25 @@
-package edu.csueastbay.cs401.pong;
+package edu.csueastbay.cs401.ejamdar;
 
-import edu.csueastbay.cs401.classic.GameController;
+
+import edu.csueastbay.cs401.pong.Collidable;
+import edu.csueastbay.cs401.pong.Collision;
+import edu.csueastbay.cs401.pong.Paddle;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
 
-public class Paddle extends Rectangle implements Collidable{
+public class JamdarPaddle extends Paddle implements Collidable {
     public static final double STARTING_SPEED = 5.0;
     private String id;
     private double speed;
     private double topBound;
     private double bottomBound;
 
-    enum Direction {UP, Down, STILL}
+    enum Direction {UP, Down, STILL, RIGHT, LEFT}
     private Direction moving;
 
 
-    public Paddle(String id, double x, double y, double width, double height, double topBound, double bottomBound) {
-        super(x, y, width, height);
+    public JamdarPaddle(String id, double x, double y, double width, double height, double topBound, double bottomBound) {
+        super(id, x, y, width, height, topBound, bottomBound);
         this.id = id;
         this.topBound = topBound;
         this.bottomBound = bottomBound;
@@ -51,13 +54,32 @@ public class Paddle extends Rectangle implements Collidable{
         if (moving == Direction.UP) {
             setY(getY() - speed);
         } else if (moving == Direction.Down) {
+            setY(getY() + speed);
+        }
+        if (getY() < topBound) setY(topBound);
+        double floor = bottomBound - getHeight();
+        if (getY() > floor) setY(floor);
+
+    }
+
+    public void move2() {
+        if (moving == Direction.UP) {
+            setY(getY() - speed);
+        } else if (moving == Direction.Down) {
             setY(getY() + speed) ;
+        } else if (moving == Direction.LEFT){
+            setX(getX()- speed);
+        } else if (moving == Direction.RIGHT){
+            setX(getX()+ speed);
         }
 
 
         if (getY() < topBound) setY(topBound);
         double floor = bottomBound - getHeight();
         if (getY() > floor) setY(floor);
+
+
+
 
     }
 
@@ -71,6 +93,14 @@ public class Paddle extends Rectangle implements Collidable{
 
     public void moveDown() {
         moving = Direction.Down;
+    }
+
+    public void moveLeft() {
+        moving = Direction.LEFT;
+    }
+
+    public void moveRight() {
+        moving = Direction.RIGHT;
     }
 
 }
